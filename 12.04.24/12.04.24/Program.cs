be0adcf1e362760f9f2bd9f1eabade8329b8e5dc
Atanas9a <godbot14@gmail.com>
2024-05-08 09:00:29 +0300

    Add files via upload

---
+++ 12.04.24/12.04.24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12._04._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
