fec261461dd420348826499c06dae33aaf705a13
Atanas9a <godbot14@gmail.com>
2024-05-08 09:07:21 +0300

    Add files via upload

---
+++ 12.04/12.04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            grupa a = new grupa();
+            a.Ime = "Atanas";
+            a.Adres = "Unas";
+            a.Ocenka = 2;
+            a.Zdrasti();
+            a.Zdravei();
+            a.Zdra();
+            grupa b = new grupa();
+            b.Ime = "Baba";
+            b.Adres = "Uvas";
+            b .Ocenka = 3;
+            b.Zdrasti();
+            b.Zdravei();
+            b.Zdra();
+        }
+    }
+}
---
+++ 12.04/12.04/grupa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._04
+{
+    internal class grupa
+    {
+       private string ime;
+       private double ocenka;
+       private string adres;
+        
+        public string Ime 
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        
+        public string Adres 
+        { 
+            get { return adres; }
+            set { adres = value; }
+        }
+        public double Ocenka 
+        {
+            get { return ocenka; }
+            set { ocenka = value; }
+        }
+        public void Zdrasti() 
+        {
+            Console.WriteLine("Zdrasti az sym {0} i sym ot {1} imam ocenka {2}.",ime,adres, ocenka);
+        }
+        public void Zdravei()
+        {
+            Console.WriteLine($"Zdrasti az sym {ime} i sym ot {adres} imam ocenka {ocenka}");
+        }
+        public void Zdra()
+        {
+            Console.WriteLine($" Zdrasti az sym " + ime + " i sym ot " +  adres + " imam ocenka " +ocenka);
+        }
+    }
+}
