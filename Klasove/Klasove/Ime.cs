d8e5592199aff515050472237ef44359dbadfb59
Atanas9a <godbot14@gmail.com>
2024-05-10 12:11:28 +0300

    Add files via upload

---
+++ Klasove/Klasove/Ime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasove
+{
+    internal class Ime
+    {
+        private String name;
+       
+        public String Name
+        {
+           get { return name; }
+           set { name = value; }
+        }
+        public void Out()
+        {
+            Console.WriteLine("ZDR ! Az sam {0} i sam na mnog dobur.", name);
+        }
+    }
+}
---
+++ Klasove/Klasove/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kak se kazvash: ");
+            string name = Console.ReadLine();
+            Ime ime = new Ime();
+            ime.Name = Console.ReadLine();
+            ime.Out();
+          
+        }
+    }
+}
